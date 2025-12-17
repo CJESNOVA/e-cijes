1074dc403a6c377ef10006815e3b2842fd1a2eb2
yokamly <collaboration.cjes@gmail.com>
2025-12-17 17:30:32 +0000

    Add . for Laravel (Dokploy)

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -60,7 +60,8 @@
             </div>
         </div>-->
 @php
-    $pagelibre1 = DB::table('pagelibres')->where('etat', 1)->where('spotlight', 1)->where('langue_id', __('id'))->first();
+    $pagelibre1 = DB::table('pagelibres')->where('id', 1)->first();
+    //$pagelibre1 = DB::table('pagelibres')->where('etat', 1)->where('spotlight', 1)->where('langue_id', __('id'))->first();
     //$pagelibre2 = DB::table('pagelibres')->where('id', 1)->where('etat', 1)->where('spotlight', 0)->where('langue_id', __('id'))->first();
 @endphp
         <!--==============================
